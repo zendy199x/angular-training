6efe6b590edf7486a0eaa07ca9756b7c2f1b943e
Nguyễn Văn Đức <nguyenduc.051194@gmail.com>
2020-07-03 03:56:50 +0700

    day 3 - data and event binding

---
+++ angular-data-binding/src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component, VERSION } from '@angular/core';
+
+@Component({
+  // tslint:disable-next-line: component-selector
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  name = 'Angular ' + VERSION.major;
+
+  inputType = 'text';
+  // DATA BINDING
+  // EVENT BINDING
+
+  user = {
+    name: 'Zendy',
+    age: 27
+  };
+
+  handler = (event) => {
+    console.log('clicked', event);
+  }
+}
---
+++ angular-data-binding/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HelloComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
