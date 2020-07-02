b49c8d7fe500fb5edd7c5cf76f0ee2adb21230e1
Nguyễn Văn Đức <nguyenduc.051194@gmail.com>
2020-07-03 02:34:04 +0700

    day 2 - explore

--- angular-train/src/app/app.module.ts
+++ angular-train/src/app/app.module.ts
@@ -1,11 +1,18 @@
+import { HelloComponent } from './hello/hello.component';
+// Typescript Module
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HiComponent } from './hi/hi.component';
 
+// Typescript Decorator
 @NgModule({
+  // Metadata: data about data
   declarations: [
-    AppComponent
+    AppComponent,
+    HelloComponent,
+    HiComponent
   ],
   imports: [
     BrowserModule
---
+++ angular-train/src/app/hello/hello.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hello',
+  template: `<span>Hello Component Works</span>`
+})
+
+export class HelloComponent {}
