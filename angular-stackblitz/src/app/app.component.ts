e953579adc3bc6b69909f6d93d13646ae786fde5
Nguyễn Văn Đức <nguyenduc.051194@gmail.com>
2020-07-03 15:00:33 +0700

    angular stackblitz.com

---
+++ angular-stackblitz/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component, VERSION } from '@angular/core';
+
+@Component({
+  // tslint:disable-next-line: component-selector
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  name = 'Angular ' + VERSION.major;
+}
